32f38738451b194b1ebe61bb7f1b095623c52d8c
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-10-05 10:21:53 -0300

    remove appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,8 +5,6 @@ import 'package:flutter_gerenciador_gastos/screens/constants/color_contant.dart'
 import 'package:flutter_gerenciador_gastos/screens/home/components/body.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-import 'components/drawer.dart';
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -22,21 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       key: _scaffoldKey,
       backgroundColor: Theme.of(context).backgroundColor,
       body: Body(),
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).backgroundColor,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: kBlackColor,
-          ),
-          onPressed: () {
-            return _scaffoldKey.currentState.openDrawer();
-          },
-          padding: EdgeInsets.only(left: 8),
-        ),
-      ),
-      drawer: drawer(context),
+
       floatingActionButton: buildSpeedDial(),
     );
   }
